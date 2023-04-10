1f3dfe4ec3ea20da9f5d2558764086bd61dfa5a2
Prakash Yogi <prakash.y@fyle.in>
2023-04-10 09:52:36 +0530

    Lecture 4 done

--- recursion/re4.py
+++ recursion/re4.py
@@ -17,3 +17,30 @@ print(f"After: {arr}")
 # 1. If the left index is less than or equal to the right index, continue. Otherwise, stop.
 # 2. Swap the left index with the right index.
 # 3. Recursively call main() with the left index increased by 1 and the right index decreased by 1. 
+
+# Reverse array recursively using single parameter
+'''
+def rev(arr, i):
+    n = len(arr)
+    if i >= n // 2:
+        return
+    arr[i], arr[n-i-1] = arr[n-i-1], arr[i]
+    rev(arr, i + 1)
+arr = [1,2,3,4,5,6,7,8,9]
+print(f"Before: {arr}")
+rev(arr, 0)
+print(f"After: {arr}")
+'''
+
+# Problem 2:
+# Write functional code to check if a string is a palindrome
+'''
+def main(string, i):
+    n = len(string)
+    if i >= n // 2:
+        return True
+    if string[i] != string[n-i-1]:
+        return False
+    return main(string, i + 1)
+print(main("racecar", 0))
+'''
