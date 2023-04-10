45749d743738747edd57deb5efdf52001dcae5d1
Prakash Yogi <prakash.y@fyle.in>
2023-04-10 08:06:55 +0530

    Added re1 lecture

---
+++ recursion/re1.py
@@ -0,0 +1,11 @@
+# Recursion is when a function calls itself until a specified condition is met
+# Example:
+'''
+def main(i):
+    print(i)
+    main(i+1)   
+'''
+# Above is an example of a recursive function.
+# It will print the value of i and then call itself again with i+1's value
+# This will continue until the program crashes. Infinite recursion like infinite loop
+
